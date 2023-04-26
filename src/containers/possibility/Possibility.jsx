37bb8814049f851de1da1a6c22d32fb54d1a7358
sal-jarrar <eng.sultanj@gmail.com>
2023-04-26 06:57:14 -0700

    containers folder

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,7 @@
+import React from "./possibility.css";
+
+const Possibility = () => {
+  return <div>Possibility</div>;
+};
+
+export default Possibility;
---
+++ src/containers/whatGBT3/WhatGBT3.jsx
@@ -0,0 +1,7 @@
+import "./whatGBT3.css";
+
+const WhatGBT3 = () => {
+  return <div>WhatGBT3</div>;
+};
+
+export default WhatGBT3;
